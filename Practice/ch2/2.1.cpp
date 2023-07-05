3a337d3c20a2df33a3c1ca2e0345378b84d02164
Ares-cz <cz0423@126.com>
2023-07-05 20:31:06 +0800

    2.1.cpp

---
+++ Practice/ch2/2.1.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 曹震 on 2023/7/5.
+//
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    cout << "My name is Zhen Cao." << endl;
+    cout << "My location is in Beijing." << endl;
+
+    return 0;
+}
